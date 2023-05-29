eb710026c99ddd53870d44c702584a20399d760f
gitkkgg <gameranger.kg@mail.ru>
2023-05-29 21:20:28 +0600

    payCredit

--- RomaPP04/KitchenWeb/Controllers/BankController.cs
+++ RomaPP04/KitchenWeb/Controllers/BankController.cs
@@ -37,6 +37,7 @@ namespace KitchenWeb.Controllers
                     connection.Open();
                     string sqlExpression = "ReadCredit";
                     SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
                     var reader = command.ExecuteReader();
                     var results = new List<Dictionary<string, object>>();
                     var cols = new List<string>();
@@ -63,7 +64,7 @@ namespace KitchenWeb.Controllers
 
         }
         [HttpPost("/CreateCredit")]
-        public async Task<IActionResult> CreateCredit([FromForm] string summa, string year, double percent, double fine, DateTime date)
+        public async Task<IActionResult> CreateCredit([FromForm] string summa, string year, double percent, double fine, DateTime date, String description)
         {
             try
             {
@@ -108,6 +109,12 @@ namespace KitchenWeb.Controllers
                         Value = date
                     };
                     command.Parameters.Add(dateParam);
+                    SqlParameter descriptionParam = new SqlParameter
+                    {
+                        ParameterName = "@description",
+                        Value = description
+                    };
+                    command.Parameters.Add(descriptionParam);
 
                     var reader = command.ExecuteReader();
                     var results = new List<Dictionary<string, object>>();
@@ -134,5 +141,103 @@ namespace KitchenWeb.Controllers
             }
 
         }
+
+        [HttpPost("/GetCreditPay")]
+        public async Task<IActionResult> GetCreditPay([FromForm] int credit_id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sqlExpression = "ReadPayByCredit_Id";
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter creditIdParam = new SqlParameter
+                    {
+                        ParameterName = "@credit_id",
+                        Value = credit_id
+                    };
+                    command.Parameters.Add(creditIdParam);
+
+                    var reader = command.ExecuteReader();
+                    var results = new List<Dictionary<string, object>>();
+                    var cols = new List<string>();
+                    for (var i = 0; i < reader.FieldCount; i++)
+                        cols.Add(reader.GetName(i));
+                    while (reader.Read())
+                        results.Add(SerializeRow(cols, reader));
+                    return new JsonResult(new
+                    {
+                        status = 1,
+                        message = "Успех",
+                        result = results
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                return new JsonResult(new
+                {
+                    status = 2,
+                    message = "Ошибка"
+                });
+            }
+
+        }
+
+
+        [HttpPost("/PayCredit")]
+        public async Task<IActionResult> PaySalary([FromForm] DateTime date, int credit_id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("CreatePay", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "CreatePay";
+                        command.Parameters.AddWithValue("@date", date);
+                        command.Parameters.AddWithValue("@credit_id", credit_id);
+                        command.Parameters.Add("@rez", SqlDbType.Int).Direction = ParameterDirection.Output;
+                        command.Parameters.Add("@total", SqlDbType.Float).Direction = ParameterDirection.Output;
+                        command.ExecuteNonQuery();
+                        int rezValue = (int)command.Parameters["@rez"].Value;
+                        double totalValue = (double)command.Parameters["@total"].Value;
+                        if (rezValue == 1)
+                        {
+                            return new JsonResult(new
+                            {
+                                status = 1,
+                                message = "Выплачено: "+totalValue
+                            });
+                        }
+                        else
+                        {
+                            return new JsonResult(new
+                            {
+                                status = 0,
+                                message = "Недостаточное денег в бюджете! Необходимо "+ totalValue,
+                            });
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return new JsonResult(new
+                {
+                    status = 2,
+                    message = "Ошибка"
+                });
+            }
+
+        }
+
+
+
     }
 }
